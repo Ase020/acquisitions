55214b7c85a61d04d788302a3bb2c9d270e7910b
felixNyalenda <felixasejunior@gmail.com>
2025-10-19 18:08:40 +0300

    Authentication

--- src/app.js
+++ src/app.js
@@ -1,10 +1,11 @@
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import express from 'express';
-import helmet from "helmet";
-import morgan from "morgan";
+import helmet from 'helmet';
+import morgan from 'morgan';
 
-import logger from "#config/logger.js";
+import logger from '#config/logger.js';
+import authRoutes from '#routes/auth.route.js';
 
 const app = express();
 
@@ -13,11 +14,31 @@ app.use(cookieParser());
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(morgan('combined', { stream: { write: (message) => logger.info(message.trim()) } }));
+app.use(
+  morgan('combined', {
+    stream: { write: message => logger.info(message.trim()) },
+  })
+);
 
 app.get('/', (req, res) => {
-    logger.info("Hello from Acquisitions");
+  logger.info('Hello from Acquisitions');
   res.status(200).send('Hello from Acquisitions');
 });
+
+app.get('/api', (req, res) => {
+  res.status(200).json({ message: 'Acquisitions API is running!' });
+});
+
+app.get('/health', (req, res) => {
+  res
+    .status(200)
+    .json({
+      status: 'OK',
+      timestamp: new Date().toISOString(),
+      uptime: process.uptime(),
+    });
+});
+
+app.use('/api/auth', authRoutes);
 
 export default app;
